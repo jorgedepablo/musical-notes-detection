6d8c2259bcf37e6e79e61a0d2957bef75b162691
Jorge De Pablo <36046965+jorgedepablo@users.noreply.github.com>
2019-11-22 18:48:59 +0100

    add function for create the octaves

--- thresholds.py
+++ thresholds.py
@@ -2,20 +2,32 @@
 # -*- coding: utf-8 -*-
 import json
 
-file = "src/notes.json"
-notes_list = ['Do', 'Do#/ReB', 'Re', 'Re#/MiB', 'Mi', 'Fa', 'Fa#/SolB', 'Sol',
-              'Sol#/LaB', 'La', 'La#/SiB', 'Si']
+FILE = "src/notes.json"
+NOTES = ['Do', 'Do#/ReB', 'Re', 'Re#/MiB', 'Mi', 'Fa', 'Fa#/SolB', 'Sol',
+         'Sol#/LaB', 'La', 'La#/SiB', 'Si']
+OCTAVES_NUM = 5
+
 def json2data(file):
     notes_Dict = {}
     try:
-        with open(file, 'r') as json_file:
+        with open(FILE, 'r') as json_file:
             notes_Dict = json.load(json_file)
         return (notes_Dict)
     except FileNotFoundError:
         print("Invalid File Name")
 
+def create_octaves(notes_Dict):
+    octaves_list = []
+    i = 1
+    for i <= OCTAVES_NUM :
+        aux_Dict = {}
+        aux_Dict = notes_Dict
+        for note in NOTES:
+            aux_Dict[note] = notes_Dict[note]*(2**(i-1))
+        octaves_list.append(aux_Dict)
+    print(octaves_list)
+
+
 if __name__ == '__main__':
-    notes_Dict = json2data(file)
-    for note in notes_list:
-        notes_Dict[note] = (notes_Dict[note], 5)
-        print(notes_Dict[note])
+    notes_Dict = json2data(FILE)
+    create_octaves(notes_Dict)
